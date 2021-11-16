f17fc2b0a84f4a5a1f0aa22c72b3f2d4d374a0dc
Isaac Benson <76756625+IsaacBenson1@users.noreply.github.com>
2021-11-15 19:16:56 -0600

    Commit 2

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import hashlib
-import datetime
-import cryptography
 import json
 import time
-import rsa
-
-
+from Crypto.PublicKey import RSA
+from Crypto.Hash import SHA256
+from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
+import random
 
 
 class Transaction:
@@ -14,47 +13,96 @@ class Transaction:
         self.privateKey = private
         self.amount = amount
 
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True, indent=4)
+
     def __str__(self):
-        return json.dumps(self)
+        return self.toJSON()
+
 
 
 class Block:
-    def __init__(self, prev, transaction):
-        self.prev = prev
-        self.transaction = transaction
-        t = time.localtime()
-        self.time = time.strftime("%H:%M:%S", t)
+    nonce = random.random() * 9999999
 
-    def getHash(self):
+    def __init__(self, prevHash, transaction):
+        self.prevHash = prevHash
+        self.transaction = transaction
+        createdAt = time.localtime()
+        self.time = time.strftime("%H:%M:%S", createdAt)
+
+    def getHash(self): # this returns a JSON string of the block object which is then converted to hexadecimal
         string = json.dumps(self)
         hashed = hashlib.sha256(string)
         return hashed.hexdigest()
 
 
 class Chain:
-    def __init__(self, chain, length):
-        self.chain = [Block(None, Transaction('Genesis', 'Naruto', 100))]
-        self.length = 0
+    def __init__(self, chain=Block(None, Transaction('Genesis', 'Naruto', 100))):
+        self.chain = [chain]
 
     def getLast(self):
-        return self.chain[self.length]
+        return self.chain[len(self.chain)-1]
 
-    def addBlock(self, transaction, public, private):
-        verify = hashlib.
+    def mine(self, nonce):
+        solution = 1
+        print('mining...')
+        while True:
+            md5 = hashlib.md5()
+            md5.update(str(nonce+solution))
+            possibleSolution = md5.hexdigest()
+            if possibleSolution[0:4] == '0000':
+                print('Solution found. You earned some coin!')
+                return possibleSolution
+
+            solution += 1
+
+    def addBlock(self, transaction, senderPublicKey, signature):
+        toBeVerified = hashlib.sha256(str(transaction))
+        verifier = PKCS115_SigScheme(senderPublicKey)
+        try:
+            verifier.verify(toBeVerified, signature)
+            self.chain.append(Block(self.getLast().getHash(), transaction))
+            print("Verification complete. Block successfully added! :)")
+
+        except ValueError:
+            print("Invalid block :(")
+            return 0
 
 
 myChain = Chain()
 
 
 class Wallet:
-    def __init__(self, public, private):
-        self.public = rsa.PublicKey()
-        self.private = rsa.PrivateKey
+
+    def __init__(self, keyPair=RSA.generate(bits=2048)):
+        self.keyPair = keyPair
+        self.publicKey = keyPair.public_key()
+        self.signer = PKCS115_SigScheme(keyPair)
 
     def sendMoney(self, amount, recieverpublickey):
-        transaction = Transaction(amount, self.public, recieverpublickey)
-        sign = rsa.sign(transaction, self.private, 'SHA256')
-        myChain.addBlock(transaction, self.public, sign)
+        transaction = Transaction(amount, self.publicKey, recieverpublickey)
+        myHash = hashlib.sha256(str(transaction).encode())
+        signature = self.signer.sign(myHash)
+        myChain.addBlock(transaction, self.publicKey, signature)
+
+
+isaac = Wallet()
+forrest = Wallet()
+owen = Wallet()
+berg = Wallet()
+
+
+isaac.sendMoney(100, forrest.publicKey)
+forrest.sendMoney(50, owen.publicKey)
+owen.sendMoney(25, berg.publicKey)
+
+
+
+
+
+
+
+
 
 
 
